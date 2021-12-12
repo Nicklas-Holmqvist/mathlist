601eafd6d7ebfede19a33f7228ab9af92dcefc12
Nicklas-Holmqvist <nicklas_holmqvist@outlook.com>
2021-12-12 09:13:02 +0100

    almost done, gonna redone numbers

--- src/Numbers.tsx
+++ src/Numbers.tsx
@@ -3,14 +3,19 @@ import React, { useEffect, useState } from 'react'
 
 const Numbers = () => {
     const [randomNumbers, setRandomNumbers] = useState<number[]>([])
-    const [userAnswers, setUserAnswers] = useState<string[]>([])
-    const [userNumber, setUserNumber] = useState<number>(7)
+    const [userAnswers, setUserAnswers] = useState<boolean[]>([])
+    const [numberTable, setNumberTable] = useState<number>(7)
     const [answers, setAnswers] = useState<boolean>(false)
+    const [resetInput, setResetInput] = useState<boolean>(false)
     const numberList:number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
     useEffect(()=> {
-        tenNumbers()
-    },[userNumber])
+        tenNumbers()        
+    },[numberTable])
+
+    useEffect(() => {
+        setResetInput(false)
+    }, [resetInput])
     
     function tenNumbers() {
         setRandomNumbers([])
@@ -28,69 +33,83 @@ const Numbers = () => {
     }
 
     function division() {
-       return random() * userNumber
+       return random() * numberTable
     }
-
-    function onChange(e:any) {
-        createCorrectionList(e)        
-    }
-
-    function correct(number:number, value:number) {        
-        if(number === value) return 'Rätt' ; return 'Fel'
-    }
-
-    function createCorrectionList(e:any) {
-        const value = parseFloat(e.target.value)
-        const id = e.target.id
-
-        const number = randomNumbers[id] / userNumber
-        const newUserAnswers = [...userAnswers]
-
-        newUserAnswers[id] = correct(number, value)
-        setUserAnswers(newUserAnswers)
-        console.log()
-    }
-    
-
-    const viewNumbers = randomNumbers.map((number, i) => (
-        <div key={i} className="flex w-20 between">
-            <div className="flex w-10 number">
-                <p>{number}</p>
-                <p>/</p>
-                <p>{userNumber}</p>
-                <p>=</p>
-            </div>
-            <div className="bottom">
-                <TextField className="w-5 ml-1 left" type="text" name="answer" id={i.toString()} onChange={onChange} />
-            </div>
-            <p className="w-5">{ answers ? userAnswers[i] : ""}</p>
-        </div>
-    ))
-
     const numberChoice = numberList.map((number, i) => (
         <div key={i}>
             <Button variant="outlined" id={number.toString()} onClick={(e) => choice(e)}>{number}</Button>
         </div>
     ))
-
+    
     function choice(e:any) {
-        const value = parseInt(e.target.innerText)
-        setUserNumber(value)
+        const value = parseInt(e.target.innerText)          
+        setResetInput(true)   
+        setNumberTable(value) 
+        setAnswers(false)
     }
+    
+    
+    function onChange(e:any) {        
+        createCorrectionList(e)      
+    }
+    
+    function createCorrectionList(e:any) {
+        const value = parseFloat(e.target.value)
+        const id = e.target.id
+        
+        const number = randomNumbers[id] / numberTable
+        
+        const newUserAnswers = [...userAnswers]        
+        newUserAnswers[id] = correct(number, value)
+        setUserAnswers(newUserAnswers)
+    }
+        
+        
+    function correct(number:number, value:number) {        
+        if(number === value) return true ; return false
+    }
+        
+    const viewNumbers = randomNumbers.map((number, i) => (
+        <div key={i} className="flex w-20 between">
+            <div className="flex w-10 number">
+                <p>{number}</p>
+                <p>/</p>
+                <p>{numberTable}</p>
+                <p>=</p>
+            </div>
+            {resetInput ?  
+            ""
+            : 
+            <div className="bottom">
+                <TextField
+                    className="w-5 ml-1 left" 
+                    type="text" 
+                    name="answer" 
+                    id={i.toString()} 
+                    onChange={onChange} 
+                />
+            </div>
+            }           
+            <p 
+                className={`w-5 ${userAnswers[i] ? "right" : "wrong"}`}>{ answers ? (userAnswers[i] ? 'RÄTT' : 'X') : ""}
+            </p>
+        </div>
+    ))
 
-    function viewCorrect() {            
+    function viewCorrect() {
         const answers = userAnswers.length
         const numbers = randomNumbers.length
 
         if(answers !== numbers) {
             return
         }
-        setAnswers(true)        
+        setAnswers(true)
     }
 
     return (
         <div className="flex center column">
             <h1>Drilla matte!</h1>
+            <p className="mb-1">Välj tabell</p>
             <div className="flex mb-2">
                 {numberChoice}
             </div>
